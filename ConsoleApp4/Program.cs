d27238d1765e69b8d968a7ca4669dbfa420b12e7
Ahmad Akel <ahmad.oudai1999@gmail.com>
2019-03-12 19:03:43 +0100

    'aa'

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[6];
+            number[0] = 1;
+            number[1] = 2;
+            number[2] = 3;
+            number[3] = 4;
+            number[4] = 5;
+            number[5] = 6;
+            for (int i = 0; i < number.Length ; i++)
+            {
+                Console.WriteLine("the result is :" +number [i]+1 );
+            }
+            Console.ReadKey();
+        }
+    }
+}
